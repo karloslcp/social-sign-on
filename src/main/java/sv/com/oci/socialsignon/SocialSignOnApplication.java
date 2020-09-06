4e9e5e2f42b46e9a78dbc080e5e1aed488914e74
Karlos Chita <karloslcp@gmail.com>
2020-09-06 15:19:12 -0500

    Add base source code from Spring Initializer

---
+++ src/main/java/sv/com/oci/socialsignon/SocialSignOnApplication.java
@@ -0,0 +1,13 @@
+package sv.com.oci.socialsignon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSignOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSignOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/com/oci/socialsignon/SocialSignOnApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.oci.socialsignon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSignOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
