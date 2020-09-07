fdcdccd1b8857fc9a4d7dba298c7b4f68a41f0ad
Karlos Chita <karloslcp@gmail.com>
2020-09-06 23:05:07 -0500

    Add logout button

--- src/main/java/sv/com/oci/socialsignon/WebSecurityConfig.java
+++ src/main/java/sv/com/oci/socialsignon/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
@@ -17,8 +18,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                         .antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
                 .oauth2Login();
     }
