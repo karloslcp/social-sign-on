487bd7bdf865ab094855ff0bd7f216a971069f42
Karlos Chita <karloslcp@gmail.com>
2020-09-07 12:34:09 -0500

    Add GET method support for logout endpoint

--- src/main/java/sv/com/oci/socialsignon/WebSecurityConfig.java
+++ src/main/java/sv/com/oci/socialsignon/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
@@ -19,6 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                         .anyRequest().authenticated()
                 )
                 .logout(l -> l
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutSuccessUrl("/").permitAll()
                 )
                 .exceptionHandling(e -> e
